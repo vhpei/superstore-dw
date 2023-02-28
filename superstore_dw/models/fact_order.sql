e6c18c36d669248f09634cef90902de493ec9d03
vitorhmpeixotojogos@gmail.com <vitorhmpeixotojogos@gmail.com>
2023-02-28 00:43:10 +0000

    version 2.0

--- superstore_dw/models/fact_order.sql
+++ superstore_dw/models/fact_order.sql
@@ -5,7 +5,16 @@ with fact as(
     da2.sk_date as sk_shipped_date,
     dc.sk_customer,
     geo.sk_geography,
-    emp.sk_employee
+    emp.sk_employee,
+    CASE
+    WHEN now()::date < sh.ship_date and now()::date > o.order_date THEN 'Ordered'
+    WHEN now()::date > sh.ship_date THEN 'Shipped'
+    END AS order_status,
+    CASE
+    WHEN now()::date < sh.ship_date and now()::date > o.order_date THEN sh.ship_date - now()::date
+    ELSE '0'
+    END AS time_to_ship_days,
+    shmo.ship_mode
     
 from {{ source("norm", "t_order") }} o
 join {{ source("dw", "dim_date") }} da on da.date = o.order_date
@@ -19,6 +28,18 @@ join {{ source("norm", "t_country") }} cc on ci.country_id = cc.country_id
 join {{ source("dw", "dim_geography") }} geo on geo.city = ci.city and geo.state = s.state and geo.region = r.region and geo.country = cc.country
 join {{ source("norm", "t_employee") }} em on r.region_id = em.region_id
 join {{ source("dw", "dim_employee") }} emp on emp.employee_id = em.employee_id
+join {{ source("norm", "t_ship_mode") }} shmo on shmo.ship_mode_id = sh.ship_mode_id
+),
+order_line as (
+    select o.order_id, count(ol.order_id) as nr_of_order_lines, sum(ol.quantity) as quantity, sum(ol.sales) as sales, sum(ol.profit) as profit
+    from {{ source("norm", "t_order") }} o
+    join {{ source("norm", "t_order_line") }} ol on ol.order_id = o.order_id
+    group by o.order_id
+),
+factual as(
+    select f.*, ol.nr_of_order_lines, ol.quantity, ol.sales, ol.profit
+    from fact f
+    join order_line ol on ol.order_id = f.order_id
 )
 select *, now() as created_at
-from fact
+from factual
